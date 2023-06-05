12589aad336dc640ac8d26967413b5421a53c13f
Marlon <marlon.lalangui@epn.edu.ec>
2023-06-04 19:53:04 -0500

    cambio index

--- index.js
+++ index.js
@@ -3,34 +3,107 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.get('/',(req,res)=>{
-    res.send("Landing page - Grupo #")
+app.get('/', (req, res) => {
+    res.send("Landing page - Grupo #2")
 })
 
-app.get('/integrantes',(req,res)=>{
+app.get('/integrantes', (req, res) => {
     res.json([
         {
-            "nombre":"Marlon",
-            "apellido":"Lalangui",
-            "edad":34
+            "nombre": "Marlon",
+            "apellido": "Lalangui",
+            "edad": 20
         },
         {
-            "nombre":"Juan",
-            "apellido":"Álvear",
-            "edad":38
+            "nombre": "Josue",
+            "apellido": "Salazar",
+            "edad": 21
         },
         {
-            "nombre":"Ximena",
-            "apellido":"Salazar",
-            "edad":33
+            "nombre": "Melani",
+            "apellido": "Molina",
+            "edad": 20
+        },
+        {
+            "nombre": "Wilson",
+            "apellido": "Guayanay",
+            "edad": 22
+        },
+        {
+            "nombre": "Katherine",
+            "apellido": "Cangahuamin",
+            "edad": 22
         }
     ])
 })
 
-app.get('/products',(req,res)=>{
+app.get('/products', (req, res) => {
     res.send(`
-            <h1>Catálogo de productos</h1>
-                <p>Bienvenidos</p>
+    <h1>Catálogo de productos</h1>
+    <p>Bienvenidos</p>
+    
+       <style>
+            body {
+                text-align: center;
+            }
+    
+            h1 {
+                color: #333;
+            }
+    
+            .product-list {
+                list-style-type: none;
+                padding: 0;
+                margin: 20px 0;
+            }
+    
+            .product-list li {
+                margin-bottom: 10px;
+            }
+    
+            .product-list li span {
+                display: inline-block;
+                width: 100px;
+                text-align: left;
+            }
+    
+            .btn {
+                display: inline-block;
+                padding: 10px 20px;
+                background-color: #333;
+                color: #fff;
+                text-decoration: none;
+                margin: 10px;
+            }
+    
+            .btn:hover {
+                background-color: #555;
+            }
+        </style>
+    <body>
+    
+        <ul class="product-list">
+            <li>
+                <span>Producto 1:</span>
+                <span>$10.00</span>
+            </li>
+            <li>
+                <span>Producto 2:</span>
+                <span>$15.00</span>
+            </li>
+            <li>
+                <span>Producto 3:</span>
+                <span>$20.00</span>
+            </li>
+            <!-- Agrega más productos aquí -->
+    
+        </ul>
+    
+        <a href="#" class="btn">Guardar compra</a>
+        <a href="#" class="btn">Cancelar compra</a>
+    </body>
+         </html>
+                
     `)
 })
 
